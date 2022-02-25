e429e9e2ce6b485393722c5966a3a7efaed78f98
chaitanya peddakotla <chaitanya.peddakotla@idsnext.com>
2022-02-25 19:52:39 +0530

    changes pushed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './public/login/login.component';
+import { SignupComponent } from './public/signup/signup.component';
+import { PodcastListComponent } from './private/podcast-list/podcast-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignupComponent,
+    PodcastListComponent
   ],
   imports: [
     BrowserModule,
