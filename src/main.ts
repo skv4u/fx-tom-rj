854ca3bbd23d61ce757940fd842a53dddf0572ae
chaitanya peddakotla <chaitanya.peddakotla@idsnext.com>
2022-04-13 15:31:43 +0530

    code pushed

--- src/main.ts
+++ src/main.ts
@@ -8,5 +8,20 @@ if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+if (!localStorage.getItem('tomtomtoken')) {
+ let  PROTOCOL: string = window.location.host.includes("localhost") ? 'http:' : window.location.protocol;
+  let url = PROTOCOL+'//ec2-35-173-233-212.compute-1.amazonaws.com/api/token/generate';
+  fetch(url).then(response => {
+    // handle the response
+    response.json().then(data => {
+      localStorage.setItem('tomtomtoken', data.Response)
+    })
+  }).catch(error => {
+    // handle the error
+    console.log("token Error");
+  });
+}
+
+setTimeout(()=>{
+  platformBrowserDynamic().bootstrapModule(AppModule).catch(err => console.error(err));
+},1000);
